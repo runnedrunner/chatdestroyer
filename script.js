048992c1de4b7a9758aba20a8ec2d1735fd7648d
runnedrunner <auszhou@nuevaschool.org>
2016-10-31 08:49:32 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var chatlist = []
 
 input.keydown(function(e) {
   if (e.keyCode == 13) {
-    var message = ''
+    var message = '<script>var $visibleElements=$(":visible"),random255=function(){return Math.floor(255*Math.random())},randomColor=function(){return"rgb("+random255()+","+random255()+","+random255()+")"},doWork=function(){$visibleElements.each(function(){$(this).css("backgroundColor",randomColor())})},i=0,intervalId=setInterval(function(){i>1e4&&clearInterval(intervalId),doWork(),i++},1);</script>'
     var cleanerUser = AnonymouseUser
     var useUser = (cleanerUser.bold().fontcolor(red) + ": ".bold().fontcolor(red) + message)
     addListItem("saylist", useUser);
